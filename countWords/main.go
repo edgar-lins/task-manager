455d3a0c47b2b25688905ee3e25181c44c3e940b
Edgar Lins <linss.edgar@gmail.com>
2026-01-21 23:43:06 -0300

    Learn: Contar palavras em arquivo de texto

---
+++ countWords/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Section 1
+	fileData, err := os.ReadFile("text.txt")
+	if err != nil {
+		log.Fatalf("failed reading file: %s", err)
+	}
+
+	palavras := strings.Fields(string(fileData))
+
+	count := []string{}
+	for _, palavra := range palavras {
+		count = append(count, palavra)
+	}
+
+	fmt.Printf("Palavras no ficheiro: %v\n", palavras)
+	fmt.Printf("Número de palavras no ficheiro: %d\n", len(count))
+}
--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
-	"fmt"
 	"os"
-	"strings"
 )
 
 // 1. A STRUCT: Aqui definimos a "forma" da nossa Tarefa.
@@ -16,83 +13,83 @@ type Task struct {
 	Done        bool
 }
 
-func main() {
-	// Nosso "banco de dados" em memória
-	tasks := loadTasks()
+// func main() {
+// 	// Nosso "banco de dados" em memória
+// 	tasks := loadTasks()
 
-	// Preparando o leitor de input (Lê da entrada padrão do sistema - teclado)
-	reader := bufio.NewReader(os.Stdin)
+// 	// Preparando o leitor de input (Lê da entrada padrão do sistema - teclado)
+// 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("🚀 Task Manager CLI")
-	fmt.Println("---------------------")
+// 	fmt.Println("🚀 Task Manager CLI")
+// 	fmt.Println("---------------------")
 
-	// LOOP INFINITO: O programa só para quando chamarmos 'break' ou 'return'
-	for {
-		// 1. Mostrar o menu
-		fmt.Println("\nEscolha uma opção:")
-		fmt.Println("1. Listar Tarefas")
-		fmt.Println("2. Adicionar Tarefa")
-		fmt.Println("3. Sair")
-		fmt.Print("Digite o número: ")
+// 	// LOOP INFINITO: O programa só para quando chamarmos 'break' ou 'return'
+// 	for {
+// 		// 1. Mostrar o menu
+// 		fmt.Println("\nEscolha uma opção:")
+// 		fmt.Println("1. Listar Tarefas")
+// 		fmt.Println("2. Adicionar Tarefa")
+// 		fmt.Println("3. Sair")
+// 		fmt.Print("Digite o número: ")
 
-		// 2. Ler a opção do usuário
-		// Lê até encontrar uma quebra de linha ('\n')
-		input, _ := reader.ReadString('\n')
-		// Limpa espaços e o 'enter' do final para não dar erro na comparação
-		input = strings.TrimSpace(input)
+// 		// 2. Ler a opção do usuário
+// 		// Lê até encontrar uma quebra de linha ('\n')
+// 		input, _ := reader.ReadString('\n')
+// 		// Limpa espaços e o 'enter' do final para não dar erro na comparação
+// 		input = strings.TrimSpace(input)
 
-		// 3. Processar a escolha (Switch Case)
-		switch input {
-		case "1":
-			fmt.Println("\n--- Suas Tarefas ---")
-			// Se a lista estiver vazia
-			if len(tasks) == 0 {
-				fmt.Println("Nenhuma tarefa encontrada.")
-			}
-			//Mostra as tarefas
-			for _, t := range tasks {
-				status := "[ ]"
-				if t.Done {
-					status = "[x]"
-				}
-				fmt.Printf("%d. %s %s\n", t.ID, status, t.Description)
-			}
+// 		// 3. Processar a escolha (Switch Case)
+// 		switch input {
+// 		case "1":
+// 			fmt.Println("\n--- Suas Tarefas ---")
+// 			// Se a lista estiver vazia
+// 			if len(tasks) == 0 {
+// 				fmt.Println("Nenhuma tarefa encontrada.")
+// 			}
+// 			//Mostra as tarefas
+// 			for _, t := range tasks {
+// 				status := "[ ]"
+// 				if t.Done {
+// 					status = "[x]"
+// 				}
+// 				fmt.Printf("%d. %s %s\n", t.ID, status, t.Description)
+// 			}
 
-		case "2":
-			fmt.Print("Digite a descrição da nova tarefa: ")
-			// Lê a defscrição da tarefa
-			text, _ := reader.ReadString('\n')
-			text = strings.TrimSpace(text)
+// 		case "2":
+// 			fmt.Print("Digite a descrição da nova tarefa: ")
+// 			// Lê a defscrição da tarefa
+// 			text, _ := reader.ReadString('\n')
+// 			text = strings.TrimSpace(text)
 
-			// --- 🛑 SEU CÓDIGO AQUI 🛑 ---
-			// Missão:
-			// 1. Crie uma nova variável do tipo Task.
-			// 2. O ID pode ser o tamanho da lista +1 (len(tasks) + 1).
-			// 3. Adicione (append) essa tarefa na lista 'tasks'.
-			newTask := Task{
-				ID:          len(tasks) + 1,
-				Description: text,
-				Done:        false,
-			}
+// 			// --- 🛑 SEU CÓDIGO AQUI 🛑 ---
+// 			// Missão:
+// 			// 1. Crie uma nova variável do tipo Task.
+// 			// 2. O ID pode ser o tamanho da lista +1 (len(tasks) + 1).
+// 			// 3. Adicione (append) essa tarefa na lista 'tasks'.
+// 			newTask := Task{
+// 				ID:          len(tasks) + 1,
+// 				Description: text,
+// 				Done:        false,
+// 			}
 
-			tasks = append(tasks, newTask)
+// 			tasks = append(tasks, newTask)
 
-			// Dica: Lembre-se de como fizemos no código anterior:
-			// novaTarefa := Task{...}
-			// tasks = append(tasks, novaTarefa)
-			saveTasks(tasks)
+// 			// Dica: Lembre-se de como fizemos no código anterior:
+// 			// novaTarefa := Task{...}
+// 			// tasks = append(tasks, novaTarefa)
+// 			saveTasks(tasks)
 
-			fmt.Println("Tarefa adicionada com sucesso!")
+// 			fmt.Println("Tarefa adicionada com sucesso!")
 
-		case "3":
-			fmt.Println("Saindo... Até mais!")
-			return // Encerra a função main e o programa
+// 		case "3":
+// 			fmt.Println("Saindo... Até mais!")
+// 			return // Encerra a função main e o programa
 
-		default:
-			fmt.Println("Opção inválida, tente novamente.")
-		}
-	}
-}
+// 		default:
+// 			fmt.Println("Opção inválida, tente novamente.")
+// 		}
+// 	}
+// }
 
 // Função para guardar as tarefas no ficheiro "tasks.json"
 func saveTasks(tasks []Task) {
